8795c662372d338155dbc3ffa0494be04ff2516e
Simon Schell <si_info@unitybox.de>
2019-11-12 20:40:05 +0100

    Small changes to limit

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -38,80 +38,86 @@ class _SettingsState extends State<SettingsPage> {
         title: Text(AppLocalization.of(context).text("SETTINGS")),
         backgroundColor: Colors.blueGrey,
       ),
-      body: ListView(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(32),
-            child: Column(
-              children: <Widget>[
-                Row(children: [
-                  Expanded(
-                      child: DropdownButton(
-                    value: _templateDropdown,
-                    items: templateButtons,
-                    onChanged: (int newValue) {
-                      setState(() {
-                        _templateDropdown = newValue;
-                      });
-                    },
-                  ))
-                ]),
-                Row(children: [
-                  Expanded(child: RaisedButton(child: Text(AppLocalization.of(context).text("LOAD-TEMPLATE")),
-                    onPressed: () => aspects.loadTemplate(_templateDropdown),))
-                ]),
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(32),
-            child: Row(
-              children: <Widget>[
-                Text(AppLocalization.of(context).text("MAX-VALUE")),
-                Expanded(
-                    child: Padding(
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    onChanged: (String input) {
-                      setState(() {
-                       aspects.updateLimit(double.parse(input).abs());
-                      });
-                    },
-                  ),
-                  padding: EdgeInsets.fromLTRB(32, 0, 0, 0),
-                )),
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(32),
-            child: Row(
-              children: <Widget>[
-                Text(AppLocalization.of(context).text("LOCALE-SETTING")),
-                Expanded(
-                  child: Padding(
-                    child: DropdownButton(
-                      value: _localeDropdown,
-                      onChanged: (Locale locale) {
+      body: Builder(
+        builder: (context) => ListView(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(32),
+              child: Column(
+                children: <Widget>[
+                  Row(children: [
+                    Expanded(
+                        child: DropdownButton(
+                      value: _templateDropdown,
+                      items: templateButtons,
+                      onChanged: (int newValue) {
                         setState(() {
-                          localizationChangeProvider.onLocaleChanged(locale);
-                          _localeDropdown = locale;
+                          _templateDropdown = newValue;
                         });
                       },
-                      items: localizationChangeProvider.supportedLocales().map<DropdownMenuItem<Locale>>((locale) {
-                        return DropdownMenuItem<Locale>(
-                          child: Text(localizationChangeProvider.namesForLanguageCodes[locale.languageCode]),
-                          value: locale
-                        );
-                      }).toList(),
+                    ))
+                  ]),
+                  Row(children: [
+                    Expanded(child: RaisedButton(child: Text(AppLocalization.of(context).text("LOAD-TEMPLATE")),
+                      onPressed: () => aspects.loadTemplate(_templateDropdown),))
+                  ]),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(32),
+              child: Row(
+                children: <Widget>[
+                  Text(AppLocalization.of(context).text("MAX-VALUE")),
+                  Expanded(
+                      child: Padding(
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      onSubmitted: (String input) {
+                        setState(() {
+                          double converted = double.parse(input).abs();
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text(AppLocalization.of(context).text("LIMIT-CHANGED") + converted.toString())
+                          ));
+                        aspects.updateLimit(converted);
+                        });
+                      },
                     ),
                     padding: EdgeInsets.fromLTRB(32, 0, 0, 0),
-                  ),
-                )
-              ],
+                  )),
+                ],
+              ),
             ),
-          ),
-        ],
+            Container(
+              padding: const EdgeInsets.all(32),
+              child: Row(
+                children: <Widget>[
+                  Text(AppLocalization.of(context).text("LOCALE-SETTING")),
+                  Expanded(
+                    child: Padding(
+                      child: DropdownButton(
+                        value: _localeDropdown,
+                        onChanged: (Locale locale) {
+                          setState(() {
+                            localizationChangeProvider.onLocaleChanged(locale);
+                            _localeDropdown = locale;
+                          });
+                        },
+                        items: localizationChangeProvider.supportedLocales().map<DropdownMenuItem<Locale>>((locale) {
+                          return DropdownMenuItem<Locale>(
+                            child: Text(localizationChangeProvider.namesForLanguageCodes[locale.languageCode]),
+                            value: locale
+                          );
+                        }).toList(),
+                      ),
+                      padding: EdgeInsets.fromLTRB(32, 0, 0, 0),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
